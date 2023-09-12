3649c02306b59211440bdf0dde94252a5be89d0d
Solemark <larco9mason@gmail.com>
2023-09-12 22:18:57 +1000

    Remove boilerplate add()

--- palindrome/src/main.zig
+++ palindrome/src/main.zig
@@ -1,10 +1,6 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
 export fn palindrome(input: [*]const u8, length: usize) bool {
     var x: usize = 0;
     var y: usize = length - 1;
