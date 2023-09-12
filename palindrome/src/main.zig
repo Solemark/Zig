6fa3d9de39c761da51e3f4c4df076b23f112adb0
Solemark <larco9mason@gmail.com>
2023-09-12 22:17:31 +1000

    Zig practice, initial commit

---
+++ palindrome/src/main.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const testing = std.testing;
+
+export fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
+export fn palindrome(input: [*]const u8, length: usize) bool {
+    var x: usize = 0;
+    var y: usize = length - 1;
+    while (x < y) {
+        if (input[x] != input[y]) {
+            return false;
+        }
+        x += 1;
+        y -= 1;
+    }
+    return true;
+}
+
+test "test IS palindrome" {
+    const input = "DAD";
+    try testing.expect(palindrome(input, input.len) == true);
+    const inputB = "ABCDCBA";
+    try testing.expect(palindrome(inputB, inputB.len) == true);
+}
+
+test "test NOT palindrome" {
+    const input = "Dad";
+    try testing.expect(palindrome(input, input.len) == false);
+    const inputB = "ABCDcba";
+    try testing.expect(palindrome(inputB, inputB.len) == false);
+}
