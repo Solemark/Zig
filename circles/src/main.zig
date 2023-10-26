32cccf48767b5e7fcecfa72f9834acf1abeae870
Solemark <larco9mason@gmail.com>
2023-10-26 22:23:48 +1000

    add guard

--- circles/src/main.zig
+++ circles/src/main.zig
@@ -2,9 +2,11 @@ const std = @import("std");
 const math = std.math;
 
 pub export fn calc_area(radius: f32) f32 {
+    if (radius <= 0) return 0.0;
     return math.pi * (math.pow(f32, radius, 2));
 }
 
 pub export fn calc_circ(radius: f32) f32 {
+    if (radius <= 0) return 0.0;
     return 2 * math.pi * radius;
 }
